42257c6604940d6c11161daca036a5c5b62bb266
Aceep <alycia.gautier@laposte.net>
2024-03-11 17:11:27 +0100

    er

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -2,58 +2,13 @@
 
 echo "[DB config] Configuring MariaDB..."
 
-if [ ! -d "/var/log/mysql" ]
-then
-    echo "[DB config] Creating directory for binary log files..."
-    mkdir -p /var/log/mysql
-    chown -R mysql:mysql /var/log/mysql
+#!/bin/sh
+
+# Check if the mysql database exists
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    # Initialize MariaDB
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
-if [ ! -d "/var/log/mysql" ]
-then
-    echo "[DB config] Creating directory for binary log files..."
-    mkdir -p /var/log/mysql
-    chown -R mysql:mysql /var/log/mysql
-fi
-if [ ! -d "/run/mysqld" ]
-then
-	echo "[DB config] Granting MariaDB daemon run permissions..."
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
-
-if [ -d "/var/lib/mysql/mysql" ]
-then
-	echo "[DB config] MariaDB already configured."
-else
-	echo "[DB config] Installing MySQL Data Directory..."
-	chown -R mysql:mysql /var/lib/mysql
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
-	echo "[DB config] MySQL Data Directory done."
-
-	echo "[DB config] Configuring MySQL..."
-	TMP=/tmp/.tmpfile
-
-	echo "USE mysql;" > ${TMP}
-	echo "FLUSH PRIVILEGES;" >> ${TMP}
-	echo "DELETE FROM mysql.user WHERE User='';" >> ${TMP}
-	echo "DROP DATABASE IF EXISTS test;" >> ${TMP}
-	echo "DELETE FROM mysql.db WHERE Db='test';" >> ${TMP}
-	echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" >> ${TMP}
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';" >> ${TMP}
-	echo "CREATE DATABASE ${WP_DB_NAME};" >> ${TMP}
-	echo "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
-	echo "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';" >> ${TMP}
-	echo "FLUSH PRIVILEGES;" >> ${TMP}
-
-	# Alpine does not come with service or rc-service,
-	# so we cannot use: service mysql start
-	# We might be able to install with: apk add openrc
-	# But we can also manually start and configure the mysql daemon:
-	/usr/bin/mysqld --user=mysql --bootstrap < ${TMP}
-	rm -f ${TMP}
-	echo "[DB config] MySQL configuration done."
-fi
-
-echo "[DB config] Starting MariaDB daemon on port 3306."
-exec /usr/bin/mysqld --user=mysql --console
+# Start MariaDB
+exec mysqld_safe --datadir=/var/lib/mysql --user=mysql
