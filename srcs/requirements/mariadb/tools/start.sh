46d55027d56136db692c56e8b3f4cfa36a63c934
Aceep <alycia.gautier@laposte.net>
2024-03-12 15:13:34 +0100

    R

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -2,13 +2,16 @@
 
 echo "[DB config] Configuring MariaDB..."
 
-#!/bin/sh
-
 # Check if the mysql database exists
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    # Initialize MariaDB
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
-fi
+echo "CREATING database: ${SQL_DATABASE}"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+sleep 1
+
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+echo "Database ready"
 
 # Start MariaDB
-exec mysqld_safe --datadir=/var/lib/mysql --user=mysql
+#exec mysqld_safe --datadir=/var/lib/mysql --user=mysql
